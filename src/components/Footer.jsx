c640ae53e55ffeee1e5269e7cc01de6f25f0f826
ariswj29 <ariswj29@gmail.com>
2024-10-23 23:13:15 +0700

    feat: create configuration, navbar & footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,65 @@
+import {
+  FaSquareEnvelope,
+  FaSquareFacebook,
+  FaSquareInstagram,
+  FaSquareTwitter,
+} from "react-icons/fa6";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="about">
+          <h1>MyBooks</h1>
+          <p>
+            MyBooks is a website that provides a variety of books that you can
+            buy. With MyBooks you can buy books easily and quickly. MyBooks
+            provides a variety of books ranging from novels, comics, and
+            textbooks. MyBooks is the best place to buy books.
+          </p>
+        </div>
+        <div className="links">
+          <h2>Links</h2>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/products">Products</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div className="socials">
+          <h2>Contact</h2>
+          <h4 style={{ fontWeight: "normal", marginTop: "3px" }}>
+            Jl. Ir. H. Juanda, Citarum, Kec. Bandung Wetan, Kota Bandung, Jawa
+            Barat 40116
+          </h4>
+          <div className="socials-title">
+            <a href="https://gmail.com/vercel">
+              <FaSquareEnvelope size={30} />
+            </a>
+            <a href="https://twitter.com/vercel">
+              <FaSquareTwitter size={30} />
+            </a>
+            <a href="https://facebook.com/vercel">
+              <FaSquareFacebook size={30} />
+            </a>
+            <a href="https://instagram.com/vercel">
+              <FaSquareInstagram size={30} />
+            </a>
+          </div>
+        </div>
+      </div>
+      <h3 style={{ marginTop: "3rem" }}>
+        © 2024 MyBooks. All rights reserved.
+      </h3>
+    </footer>
+  );
+};
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+export const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-content">
+        <h1>MyBooks</h1>
+        <div className="links">
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/products">Products</a>
+          <a href="/contact">Contact</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
